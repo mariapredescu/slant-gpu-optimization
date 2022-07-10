dada035f43d46fbf66853702401a23b0ef74c473
Maria Predescu <37122751+mariapredescu@users.noreply.github.com>
2022-07-10 12:29:30 +0300

    added cuda mps data and graphs

---
+++ CUDA-MPS/run_all_batches.sh
@@ -0,0 +1,3 @@
+bash ~/slant_training/OUTPUTS/sub-OAS30049_ses-d0013_run-01_T1w/working_dir/test_all_pieces.sh &
+read -p "waiting" -t 3
+bash ~/slant_training/OUTPUTS/sub-OAS30010_ses-d0068_T1w/working_dir/test_all_pieces.sh
